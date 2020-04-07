e7e44263a5d00d36c1dd38b11a54ca4939fb2608
nhatminhDuong <nhatminhduongbk@gmail.com>
2020-04-07 15:43:30 +0700

    M2 first commit

---
+++ Module2/1_Ngon_ngu_lap_trinh_Java/src/Bai_tap/_1_Hien_thi_loi_chao.java
@@ -0,0 +1,13 @@
+package Bai_tap;
+
+import java.util.Scanner;
+
+public class _1_Hien_thi_loi_chao {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
